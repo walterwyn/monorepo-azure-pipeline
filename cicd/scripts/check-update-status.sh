b4dd7b118cfee92a0ec53829b574c2029c9db77f
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2018-10-26 17:04:48 +0200

    Accept target in environment variable

--- cicd/scripts/check-update-status.sh
+++ cicd/scripts/check-update-status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-target="$1"
+target="${MONOREPO_BUILD_TARGET:-$1}"
+[[ "$target" != "" ]] || (echo "Must specify target, as argument or in \$MONOREPO_BUILD_TARGET" && exit 1)
 head="$(git rev-list -1 HEAD)"
 tag="$(git tag | grep "$target" | tail -n 1 | tr -d '\n')"
 
