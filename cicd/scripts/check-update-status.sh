91dc42b749cdeec25f085f14856fd06afc3f51ca
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2018-10-26 17:44:12 +0200

    Use full if statement

--- cicd/scripts/check-update-status.sh
+++ cicd/scripts/check-update-status.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 
 target="${MONOREPO_BUILD_TARGET:-$1}"
-[[ "$target" != "" ]] || (echo "Must specify target, as argument or in \$MONOREPO_BUILD_TARGET"; exit 1)
+
+if [[ "$target" != "" ]]; then
+  echo "Must specify target, as argument or in \$MONOREPO_BUILD_TARGET"
+  exit 1
+fi
+
 head="$(git rev-list -1 HEAD)"
 tag="$(git tag | grep "$target" | tail -n 1 | tr -d '\n')"
 
